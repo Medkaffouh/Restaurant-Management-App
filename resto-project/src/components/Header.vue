9a3a97ea52a9d5d7e82b264be66f465885365c04
Medkaffouh <kaffouhmohamed@gmail.com>
2022-08-24 14:20:14 +0100

    make header

---
+++ resto-project/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <ul class="list">
+      <li class="listItem">Home</li>
+      <li class="listItem">Add Restaurant</li>
+      <li class="listItem">Update Restaurant</li>
+      <li class="listItem">Logout</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+<style scoped>
+.container {
+  height: 50px;
+  background-color: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 0 100px;
+  font-size: 18px;
+  justify-content: space-between;
+  position: relative;
+}
+
+.listItem {
+  display: inline;
+  margin-right: 30px;
+  font-size: 14px;
+  cursor: pointer;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.listItem:hover{
+    background-color: rgb(38, 38, 38);
+}
+</style>
--- resto-project/src/components/Home.vue
+++ resto-project/src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="container">
+    <HeaderComponent />
     <h1>Hello User, Welcome on Home Page</h1>
   </div>
 </template>
 <script>
+import HeaderComponent from "./Header.vue"
 export default {
   name: 'HomeComponent',
+  components:{
+    HeaderComponent
+  },
   mounted() {
     let user= localStorage.getItem('user-info');
     if(!user){
