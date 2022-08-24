ac43916f7c195d082fb3453ee7738087fec533f6
Medkaffouh <kaffouhmohamed@gmail.com>
2022-08-24 13:50:44 +0100

    Login API integration

--- resto-project/src/components/Login.vue
+++ resto-project/src/components/Login.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="container">
+  <div class="container">
     <h1 class="logo">RESTO</h1>
     <h1>Login</h1>
     <div class="login">
       <input type="text" v-model="email" placeholder="Enter Email" />
       <input type="password" v-model="password" placeholder="Enter Password" />
-      <button v-on:click="login()" class="button">Login</button>
+      <button v-on:click="login" class="button">Login</button>
       <p>
         <router-link to="/sign-up">Sign Up</router-link>
       </p>
@@ -13,13 +13,38 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-    name:'LoginComponent',
-}
+  name: "LoginComponent",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      let result = await axios.get(
+        `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+      );
+      console.log(result);
+      if (result.status == 200 && result.data.length>0) {
+        localStorage.setItem("user-info", JSON.stringify(result.data[0]));
+        this.$router.push({ name: "Home" });
+      }
+    },
+  },
+  mounted(){
+    let result = localStorage.getItem("user-info");
+    if(result){
+        this.$router.push({name:"Home"});
+    }
+  }
+};
 </script>
 <style scoped>
-.container{
-    text-align: center;
+.container {
+  text-align: center;
   margin-top: 60px;
 }
 .logo {
