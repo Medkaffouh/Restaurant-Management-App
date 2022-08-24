46221163ba32f390bd2f5e07555ffeddf2dd4ee5
Medkaffouh <kaffouhmohamed@gmail.com>
2022-08-24 17:01:40 +0100

    Make API for restaurant List with json server

--- resto-project/src/components/Home.vue
+++ resto-project/src/components/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="container">
     <HeaderComponent />
-    <h1>Hello User, Welcome on Home Page</h1>
+    <h1>Hello {{name}}, Welcome on Home Page</h1>
   </div>
 </template>
 <script>
 import HeaderComponent from "./Header.vue"
 export default {
   name: 'HomeComponent',
+  data(){
+    return{
+      name:''
+    }
+  },
   components:{
     HeaderComponent
   },
   mounted() {
     let user= localStorage.getItem('user-info');
+    this.name= JSON.parse(user).name;
     if(!user){
       this.$router.push({name:'Login'})
     }
